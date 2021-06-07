33dbb5bf6aef9235827cac4063ef23e58ef362ca
Anas428 <anasnadeem428@gmail.com>
2021-06-08 03:56:18 +0500

    Making and Runnimg Toasts

--- Projects/Lec 10/Sqlite/app/src/main/java/com/example/sqlite/MainActivity.java
+++ Projects/Lec 10/Sqlite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button1, button2;
+    EditText editName, editAge;
+    Switch aSwitch;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.btnadd);
+        button2 = findViewById(R.id.btnView);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Add Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Add Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
