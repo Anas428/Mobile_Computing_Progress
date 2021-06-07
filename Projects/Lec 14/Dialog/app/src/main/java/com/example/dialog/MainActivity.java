9945f7eafec024bcd19b6523639c91fdb11a1319
Anas428 <anasnadeem428@gmail.com>
2021-06-08 04:16:19 +0500

    Lecture 14 First Build

---
+++ Projects/Lec 14/Dialog/app/src/main/java/com/example/dialog/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.b1);
+    }
+
+    public void ShowDialog(View view)
+    {
+
+    }
+}
