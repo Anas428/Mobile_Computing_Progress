0385ac664025a4a2166ef0604039c005f42a1244
Anas428 <anasnadeem428@gmail.com>
2021-06-08 01:10:36 +0500

    Overrride Methods Added

--- Projects/Lec 7/ALC/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ Projects/Lec 7/ALC/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +13,37 @@ public class MainActivity extends AppCompatActivity {
     Button button;
     TextView textView;
     int count;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ALC", "OnStart Called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ALC", "OnStop Called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ALC", "OnDestroy Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ALC", "OnPause Called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ALC", "OnResume Called");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
