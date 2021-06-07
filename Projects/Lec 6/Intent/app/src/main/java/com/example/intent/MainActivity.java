57fa7447eef8bfeb66c5501bc315edd9c2c0ac50
Anas428 <anasnadeem428@gmail.com>
2021-06-08 00:39:57 +0500

    New Intent Activity

--- Projects/Lec 6/Intent/app/src/main/java/com/example/intent/MainActivity.java
+++ Projects/Lec 6/Intent/app/src/main/java/com/example/intent/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Call(View view)
+    {
+        Uri uri = Uri.parse("tel:+923209462235");
+        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+        startActivity(intent);
+    }
+
+    public void Link(View view)
+    {
+        Uri uri = Uri.parse("https://www.fb.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void BatteryLow(View view)
+    {
+
     }
 }
