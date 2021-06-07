035d8583307e5fd6df3dd64e05f23675626082b0
Anas428 <anasnadeem428@gmail.com>
2021-06-08 00:02:35 +0500

    Lecture 5 After Final running

--- Projects/Lec 5/app/src/main/java/com/example/lec_views/MainActivity.java
+++ Projects/Lec 5/app/src/main/java/com/example/lec_views/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.lec_views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sample_layout);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.b1);
+        textView = findViewById(R.id.tv1);
+    }
+
+    public void SomeOneIsThere(View view)
+    {
+        textView.setText("You Clicked the button");
     }
 }
