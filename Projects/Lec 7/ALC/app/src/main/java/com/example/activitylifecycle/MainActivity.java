0fbeaf17705cca82419aba0527e55634a44727d7
Anas428 <anasnadeem428@gmail.com>
2021-06-08 01:06:09 +0500

    Before Activity Life Cycle

--- Projects/Lec 7/ALC/app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ Projects/Lec 7/ALC/app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.activitylifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textView;
+    int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView2);
+        button = findViewById(R.id.button);
+    }
+
+    public void IncreaseNumber(View view) {
+        count = Integer.parseInt(textView.getText().toString());
+        count++;
+        textView.setText(String.valueOf(count));
     }
 }
